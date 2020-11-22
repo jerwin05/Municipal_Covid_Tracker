35af756ac707e57f3b77cb93593600df4fd9a15d
Jerwin Cabral <jerwincabral94@gmail.com>
2020-11-22 22:33:26 +0800

    create database script

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const index = require('./routes/index')
 ,express = require('express')
 ,helmet = require('helmet')
 ,mysql = require('mysql')
-,cors = require('cors')
+// ,cors = require('cors')
 ,app = express();
 
 const options={  
@@ -55,6 +55,48 @@ connection.connect((err)=>{
 });
  
 global.db = connection;
+
+var sql = `SELECT first_name FROM users;`;
+db.query(sql, function(err, result) {
+   if(err){
+    sql = `CREATE TABLE users (
+      id INT NOT NULL AUTO_INCREMENT,
+      first_name TEXT NOT NULL,
+      last_name TEXT NOT NULL,
+      middle_name TEXT NOT NULL,
+      mob_no VARCHAR(11) NOT NULL,
+      user_name VARCHAR(50) NOT NULL,
+      password VARCHAR(50) NOT NULL,
+      longitude VARCHAR(150) NULL,
+      latitude VARCHAR(150) NULL,
+      remarks VARCHAR(20) NULL,
+      PRIMARY KEY (id));`;
+    db.query(sql, function(err, result) {
+      console.log('created users');
+    });
+    sql = `CREATE TABLE admin (
+      id INT NOT NULL AUTO_INCREMENT,
+      first_name TEXT NOT NULL,
+      last_name TEXT NOT NULL,
+      middle_name TEXT NOT NULL,
+      mob_no VARCHAR(11) NOT NULL,
+      user_name VARCHAR(50) NOT NULL,
+      password VARCHAR(50) NOT NULL,
+      PRIMARY KEY (id));`;
+    db.query(sql, function(err, result) {
+      console.log('created admin');
+    });
+    sql = `CREATE TABLE announcements (
+      id INT NOT NULL AUTO_INCREMENT,
+      title VARCHAR(200) NOT NULL,
+      body VARCHAR(2000) NOT NULL,
+      date VARCHAR(50) NOT NULL,
+      PRIMARY KEY (id));`;
+    db.query(sql, function(err, result) {
+      console.log('created announcements');
+    });
+  }
+});
 
 app.use('/',index);
 app.use('/member',member);
