ed79313e40a492b454abf02857424eaea0121869
Jerwin Cabral <jerwincabral94@gmail.com>
2020-11-28 20:36:47 +0800

    fix helmet library

--- app.js
+++ app.js
@@ -42,17 +42,6 @@ app.use(
     }
   })
 );
-// app.use(helmet.dnsPrefetchControl());
-// app.use(helmet.expectCt());
-// app.use(helmet.frameguard());
-// app.use(helmet.hidePoweredBy());
-// app.use(helmet.hsts());
-// app.use(helmet.ieNoOpen());
-// app.use(helmet.noSniff());
-// app.use(helmet.permittedCrossDomainPolicies());
-// app.use(helmet.referrerPolicy());
-// app.use(helmet.xssFilter());
-
 app.use(compression()); //Compress all routes
 app.use(express.static('views'));
 app.use(express.json());
