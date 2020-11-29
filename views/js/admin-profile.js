2dcbe3fbbbc5fb9758cb69e9c67d724aaaebf997
Jerwin Cabral <jerwincabral94@gmail.com>
2020-11-29 23:33:50 +0800

    add homepage features

--- views/js/admin-profile.js
+++ views/js/admin-profile.js
@@ -1,4 +1,18 @@
 //get elements here
+const newCases=document.getElementById('newCases');
+const activeCases=document.getElementById('activeCases');
+const newCasesTitle=document.getElementById('newCasesTitle');
+const activeCasesTitle=document.getElementById('activeCasesTitle');
+const suspected=document.getElementById('suspected');
+const probable=document.getElementById('probable');
+const confirmedCases=document.getElementById('cofirmedCases');
+const testedNegative=document.getElementById('testedNegative');
+const recovered=document.getElementById('recovered');
+const death=document.getElementById('death');
+const covidUpdateDate=document.getElementById('covidUpdateDate');
+const notes=document.getElementById('notes');
+const covidPatientList=document.getElementById('covidPatientList');
+
 const overlay=document.getElementById('overlay');
 const successMessage=document.getElementById('successMessage');
 const errorMessage=document.getElementById('errorMessage');
@@ -16,6 +30,8 @@ const profileAPI_URL = (window.location.hostname === '127.0.0.1' || window.locat
 const announcementAPI_URL = (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') ? 'http://localhost:3000/announcement' : 'https://barangay-covid-map.herokuapp.com/announcement';
 const IndexUrl = (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') ? 'http://localhost:3000/index.html' : 'https://barangay-covid-map.herokuapp.com/index.html';
 const authenticateAPI_URL = (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') ? 'http://localhost:3000/authenticate-user' : 'https://barangay-covid-map.herokuapp.com/authenticate-user';
+const covidupdateAPI_URL = (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') ? 'http://localhost:3000/covid-update' : 'https://barangay-covid-map.herokuapp.com/covid-update';
+const covidpositivelistAPI_URL = (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') ? 'http://localhost:3000/covid-positive-list' : 'https://barangay-covid-map.herokuapp.com/covid-positive-list';
 
 fetch(authenticateAPI_URL,{
 }).then(response=>{
@@ -47,7 +63,76 @@ function getProfile(){
 }
 
 const getCovidUpdates=()=>{
-  // fetch()
+  fetch(covidupdateAPI_URL)
+  .then(response=>{
+    response.json()
+    .then(result=>{
+      const pre=document.createElement('pre');
+
+      activeCases.textContent=result[0].active_cases;
+      newCases.textContent=result[0].new_cases;
+      suspected.textContent=result[0].suspected;
+      probable.textContent=result[0].probable;
+      confirmedCases.textContent=result[0].confirmed_cases;
+      testedNegative.textContent=result[0].tested_negative;
+      recovered.textContent=result[0].recovered;
+      death.textContent=result[0].death;
+
+      if(result[0].new_cases>1){
+        newCasesTitle.textContent='New Cases';
+      }else{
+        newCasesTitle.textContent='New Case';
+      }
+      if(result[0].active_cases>1){
+        activeCasesTitle.textContent='Active Cases';
+      }else{
+        activeCasesTitle.textContent='Active Case';
+      }
+
+      covidUpdateDate.className='covidupdate--date';
+      const updateDate=result[0].date_updated.substring(4, 16);
+      covidUpdateDate.textContent=`As of ${updateDate}`;
+
+      pre.textContent=result[0].notes;
+      notes.appendChild(pre);
+      
+    })
+  })
+};
+
+const getPositivePatients=()=>{
+  fetch(covidpositivelistAPI_URL)
+  .then(response=>{
+    response.json()
+    .then(result=>{
+      result.forEach((element,index,array)=>{
+        const mainDiv=document.createElement('div');
+        const div1=document.createElement('div');
+        const div2=document.createElement('div');
+        const patientNumber=document.createElement('p');
+        const age=document.createElement('p');
+        const gender=document.createElement('p');
+        const barangay=document.createElement('p');
+        const status=document.createElement('p');
+
+        patientNumber.textContent=element.patient_no;
+        age.textContent=element.age;
+        gender.textContent=element.gender;
+        barangay.textContent=element.barangay;
+        status.textContent=element.status;
+
+        div2.className='covidpatientlist--patientdetails';
+        div2.appendChild(age);
+        div2.appendChild(gender);
+        div1.appendChild(patientNumber);
+        div1.appendChild(div2);
+        mainDiv.appendChild(div1);
+        mainDiv.appendChild(barangay);
+        mainDiv.appendChild(status);
+        covidPatientList.appendChild(mainDiv);
+      });
+    })
+  })
 };
 
 //get announcements from db
@@ -60,7 +145,7 @@ const getAnnouncements=()=>{
 
         const div = document.createElement('div');
         const title = document.createElement('h3');
-        const body = document.createElement('p');
+        const body = document.createElement('pre');
         const date = document.createElement('small');
         const button =document.createElement('button');
 
@@ -106,6 +191,7 @@ const getAnnouncements=()=>{
 
 getProfile();
 getCovidUpdates();
+getPositivePatients();
 getAnnouncements(); 
 
 //execute event on click of post on announcement
