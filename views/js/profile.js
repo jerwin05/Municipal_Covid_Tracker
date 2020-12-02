cd08adfbaf275be768d0306634792c7f701a9b0d
Jerwin Cabral <jerwincabral94@gmail.com>
2020-12-02 18:46:33 +0800

    fix style bugs

--- views/js/profile.js
+++ views/js/profile.js
@@ -1,15 +1,18 @@
 
 const borderBottom=document.getElementById('border--bottom');
 const main=document.getElementById('main');
+const block3=document.getElementById('block3');
 
 function openTab(pageName) {
     borderBottom.className = borderBottom.className.replace(" tab--slide", "");
     main.className = main.className.replace(" page--slide", "");
+    block3.style.display='';
 
     //update tab button border
     if(pageName=='ProfileTab'){
         borderBottom.className += " tab--slide";
         main.className += " page--slide";
+        block3.style.display='none';
     }
 }
 openTab('HomeTab');
