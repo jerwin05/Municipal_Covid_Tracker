7b150ff8cff26d95877e33aae1c9089591b5a153
Jerwin Cabral <jerwincabral94@gmail.com>
2020-12-23 22:43:27 +0800

    set profile scroll top to 0

--- views/js/profile.js
+++ views/js/profile.js
@@ -1,21 +1,26 @@
 
+const body=document.querySelector('body');
 const borderBottom=document.getElementById('border--bottom');
 const main=document.getElementById('main');
 const block3=document.getElementById('block3');
-// const html=document.querySelector('html');
+
+var scrollTop=0;
 
 function openTab(pageName) {
     borderBottom.className = borderBottom.className.replace(" tab--slide", "");
     main.className = main.className.replace(" page--slide", "");
     block3.style.display='';
-    // html.style.overflowY='scroll';
 
     //update tab button border
     if(pageName=='ProfileTab'){
         borderBottom.className += " tab--slide";
         main.className += " page--slide";
         block3.style.display='none';
-        // html.style.overflowY='hidden';
+        scrollTop=body.scrollTop;
+        body.scrollTop=0;
+    }else{
+        body.scrollTop=scrollTop;
     }
 }
+
 openTab('HomeTab');
