305ec526240c0b5dd1938ec0d7592de86f6aa86f
Jerwin Cabral <jerwincabral94@gmail.com>
2020-12-23 23:17:18 +0800

    save home scroll top

--- views/js/profile.js
+++ views/js/profile.js
@@ -4,7 +4,8 @@ const borderBottom=document.getElementById('border--bottom');
 const main=document.getElementById('main');
 const block3=document.getElementById('block3');
 
-var scrollTop=0;
+var homeScrollTop=0;
+var profileScrollTop=0;
 
 function openTab(pageName) {
     borderBottom.className = borderBottom.className.replace(" tab--slide", "");
@@ -16,10 +17,10 @@ function openTab(pageName) {
         borderBottom.className += " tab--slide";
         main.className += " page--slide";
         block3.style.display='none';
-        scrollTop=body.scrollTop;
+        homeScrollTop=body.scrollTop;
         body.scrollTop=0;
     }else{
-        body.scrollTop=scrollTop;
+        body.scrollTop=homeScrollTop;
     }
 }
 
